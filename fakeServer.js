de19fbce5a4a550e8782bb47e2b1226b695c7146
Daniel Stahl <danstahl1138@gmail.com>
2017-03-26 10:25:11 -0500

    added comments

--- fakeServer.js
+++ fakeServer.js
@@ -13,32 +13,32 @@ app.use((req, res, next)=>{
 let port = process.env.PORT || 3001;
 let scopeData=[];
 let skillData=[];
-app.get("/currentAssociates", (req, res)=>{
+app.get("/currentAssociates", (req, res)=>{ 
     res.send(availablePersonel)
 })
-app.get("/skills", (req, res)=>{
+app.get("/skills", (req, res)=>{//these are "static"
     res.send(skills)
 })
-app.get("/RCUS", (req, res)=>{
+app.get("/RCUS", (req, res)=>{//these are "static"
     res.send(RCUS)
 })
-app.get("/testSelection", (req, res)=>{
+app.get("/testSelection", (req, res)=>{//these are "static"
     res.send(testSelection)
 })
-app.get("/skillAssessment", (req, res)=>{
+app.get("/skillAssessment", (req, res)=>{//in final state use validation id
     res.send(skillData);
 })
-app.get("/scopeAssessment", (req, res)=>{
+app.get("/scopeAssessment", (req, res)=>{ //in final state use validation id
     res.send(scopeData);
 })
 
-app.post("/handleTestSubmit",  (req, res)=>{
+app.post("/handleTestSubmit",  (req, res)=>{ //in final state use validation id
     console.log(req.body);
     scopeData.push(req.body);
     res.sendStatus(200);
 })
 
-app.post("/handleAddTeamMember",  (req, res)=>{
+app.post("/handleAddTeamMember",  (req, res)=>{ //in final state use validation id
     console.log(req.body);
     skillData=req.body;
     res.sendStatus(200);
